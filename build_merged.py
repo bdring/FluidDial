515f39dbd6360a4e555887f1a67409db38a4d2f4
Mitch Bradley <wmb@firmworks.com>
2025-05-17 14:17:10 -1000

    Working toward a release action

--- build_merged.py
+++ build_merged.py
@@ -1,17 +1,23 @@
-# This script adds a "build_merged" target, used like "pio run -t build_merged",
-# that will create a combined image with both the firmware and the filesystem images
+# This script adds a "build_merged" target, used like "pio run -e cyd -t build_merged",
+# that creates a combined image with both the firmware and the filesystem images
 # The two smaller images must be built first, with "pio run" and "pio run -t buildfs"
 Import("env")
 
-cmd = '$PYTHONEXE $UPLOADER --chip esp32s3 merge_bin --output $BUILD_DIR/merged-flash.bin --flash_mode dio --flash_size 8MB '
+flash_size = env.BoardConfig().get("upload.flash_size", "detect")
+
+cmd = '$PYTHONEXE $UPLOADER --chip $BOARD_MCU merge_bin --output $BUILD_DIR/merged-flash.bin --flash_mode dio --flash_size ' + flash_size + " "
+
 for image in env.get("FLASH_EXTRA_IMAGES", []):
     cmd += image[0] + " " + env.subst(image[1]) + " "
-cmd += " 0x10000 $BUILD_DIR/firmware.bin 0x670000 $BUILD_DIR/littlefs.bin"
-#print(cmd)
-#print()
+
+filesystem_start = env.GetProjectOption("custom_filesystem_start", "Missing_custom_filesystem_start_variable")
+
+cmd += " 0x10000 $BUILD_DIR/firmware.bin " + filesystem_start + " $BUILD_DIR/littlefs.bin"
+
 env.AddCustomTarget(
-    "build_merged",
-    ["$BUILD_DIR/firmware.bin", "$BUILD_DIR/littlefs.bin"],
-    cmd
-    #"$PYTHONEXE $UPLOADER --chip esp32s3 merge_bin --output $BUILD_DIR/merged-flash.bin --flash_mode dio --flash_size 8MB 0x0000 $BUILD_DIR/bootloader.bin 0x8000 $BUILD_DIR/partitions.bin 0xe000 C:/Users/wmb/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin 0x10000 $BUILD_DIR/firmware.bin 0x670000 $BUILD_DIR/littlefs.bin"
+    name="build_merged",
+    dependencies=["$BUILD_DIR/firmware.bin"],
+    actions=["pio run -e $PIOENV -t buildfs", cmd],
+    title="Build Merged",
+    description="Build combined image with program and filesystem"
 )
