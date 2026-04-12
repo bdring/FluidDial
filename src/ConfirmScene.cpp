fde9eb39fbfa4558561584176389da8247e0ffbe
figamore <90107339+figamore@users.noreply.github.com>
2026-04-12 04:23:31 -0400

    Add modal to confirm soft resets

--- src/ConfirmScene.cpp
+++ src/ConfirmScene.cpp
@@ -1,11 +1,6 @@
 #include "Menu.h"
 #include <string>
 
-// Confirm scene needs a string to display.  It displays the string on
-// a background depicting the red and green buttons.
-// It pops when one of those buttons is pressed, or on back and flick back,
-// setting a variable to true iff the green button was pressed
-
 class ConfirmScene : public Scene {
     std::string _msg;
 
@@ -15,14 +10,24 @@ public:
     void reDisplay() {
         background();
         canvas.fillRoundRect(10, 90, 220, 60, 15, YELLOW);
-        centered_text(_msg.c_str(), 120, BLACK, MEDIUM);
+
+        size_t nl = _msg.find('\n');
+        if (nl != std::string::npos) {
+            centered_text(_msg.substr(0, nl).c_str(), 108, BLACK, MEDIUM);
+            centered_text(_msg.substr(nl + 1).c_str(), 132, BLACK, MEDIUM);
+        } else {
+            centered_text(_msg.c_str(), 120, BLACK, MEDIUM);
+        }
 
         drawButtonLegends("No", "Yes", "Back");
 
         refreshDisplay();
     }
     void onRedButtonPress() { pop_scene(nullptr); }
-    void onGreenButtonPress() { pop_scene((void*)"Confirmed"); }
+    void onGreenButtonPress() {
+        dbg_printf("ConfirmScene: Yes pressed\r\n");
+        pop_scene((void*)"Confirmed");
+    }
     void onDialButtonPress() { pop_scene(nullptr); }
 };
 ConfirmScene confirmScene;
